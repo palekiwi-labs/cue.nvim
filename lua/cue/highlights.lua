0c53ffa3884a2aba4ba9df43c3abd9a8d5cabb1c
Pawel Lisewski <dev@palekiwi.com>
2026-06-28 02:31:01 +0800

    refactor: replace vermillon with green

--- lua/cue/highlights.lua
+++ lua/cue/highlights.lua
@@ -1,17 +1,17 @@
 local M = {}
 
 local COLORS = {
-  grey       = "#4c566a",
-  dark       = "#161B22",
-  darker     = "#101010",
-  blue       = "#58a6ff",
-  cyan       = "#39c5cf",
-  orange     = "#d29922",
-  yellow     = "#e5c07b",
-  pink       = "#ff7b72",
-  purple     = "#bc8cff",
-  red        = "#f85149",
-  vermillion = "#D34516",
+  grey   = "#4c566a",
+  dark   = "#161B22",
+  darker = "#101010",
+  blue   = "#58a6ff",
+  cyan   = "#39c5cf",
+  orange = "#d29922",
+  yellow = "#e5c07b",
+  pink   = "#ff7b72",
+  purple = "#bc8cff",
+  red    = "#f85149",
+  green  = "#98C379",
 }
 
 function M.setup()
@@ -21,7 +21,7 @@ function M.setup()
   vim.api.nvim_set_hl(0, "CueCategoryPlan", { fg = COLORS.purple, bold = false })
   vim.api.nvim_set_hl(0, "CueCategoryTask", { fg = COLORS.cyan, bold = false })
   vim.api.nvim_set_hl(0, "CueCategoryTodo", { fg = COLORS.blue, bold = false })
-  vim.api.nvim_set_hl(0, "CueCategoryNote", { fg = COLORS.vermillion, bold = false })
+  vim.api.nvim_set_hl(0, "CueCategoryNote", { fg = COLORS.green, bold = false })
   vim.api.nvim_set_hl(0, "CueCategoryDoc", { fg = COLORS.orange, bold = false })
   vim.api.nvim_set_hl(0, "CueCategoryTrace", { fg = COLORS.yellow, bold = false })
   vim.api.nvim_set_hl(0, "CueCategoryBin", { fg = COLORS.red, bold = false })
